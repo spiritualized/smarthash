2b126675b2302d9e1213e226168da23896e78e6e
spiritualized <contact@spiritualized.io>
2019-03-03 21:09:18 -0500

    Convert handlers to classes

--- Handlers/save.py
+++ Handlers/save.py
@@ -1,44 +1,46 @@
 import os
 
 
-description = "Writes extracted data out to a folder"
-options = ['video-screenshots']
+class TorrentHandler:
 
-def attach_arguments(argparser):
-	pass
+	description = "Writes extracted data out to a folder"
+	options = ['video-screenshots']
 
-def check_parameters(args):
-	pass
+	def attach_arguments(self, argparser):
+		pass
 
-def early_validation(path, args):
-	pass
+	def check_parameters(self, args):
+		pass
 
-def handle(data):
+	def early_validation(self, path, data):
+		pass
 
-	meta_folder = data['path']+"_smarthash"
-	if os.path.isfile(meta_folder):
-		raise ValueError("Invalid filename")
-	if not os.path.isdir(meta_folder):
-		os.makedirs(meta_folder)
-	torrent_file_path = os.path.join(data['path']+"_smarthash", data['title'])+".torrent"
+	def handle(self, data):
 
-	if 'nfo' in data:
-		with open(os.path.join(data['path']+"_smarthash", "nfo.nfo"), "w") as file:
-			file.write(data['nfo'])
-	if 'mediainfo' in data:
-		with open(os.path.join(data['path']+"_smarthash", "mediainfo.txt"), "w") as file:
-			file.write(data['mediainfo'])
+		meta_folder = data['path']+"_smarthash"
+		if os.path.isfile(meta_folder):
+			raise ValueError("Invalid filename")
+		if not os.path.isdir(meta_folder):
+			os.makedirs(meta_folder)
+		torrent_file_path = os.path.join(data['path']+"_smarthash", data['title'])+".torrent"
 
-
-	if 'extracted_images' in data:
-		for file_images in data['extracted_images']:
-			i = 0
-			for image in file_images:
-				with open(os.path.join(data['path']+"_smarthash", "screenshot_{0}.jpeg".format(i)), "wb") as file:
-					file.write(image)
-				i += 1
+		if 'nfo' in data:
+			with open(os.path.join(data['path']+"_smarthash", "nfo.nfo"), "w") as file:
+				file.write(data['nfo'])
+		if 'mediainfo' in data:
+			with open(os.path.join(data['path']+"_smarthash", "mediainfo.txt"), "w") as file:
+				file.write(data['mediainfo'])
 
 
+		if 'extracted_images' in data:
+			for file_images in data['extracted_images']:
+				i = 0
+				for image in file_images:
+					with open(os.path.join(data['path']+"_smarthash", "screenshot_{0}.jpeg".format(i)), "wb") as file:
+						file.write(image)
+					i += 1
 
-	with open(torrent_file_path, 'wb') as handle:
-		handle.write(data['torrent_file'])
+
+
+		with open(torrent_file_path, 'wb') as handle:
+			handle.write(data['torrent_file'])
--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 
-blacklist_file_extensions = [".sfv", ".ds_store", ".md5"]	# ignore these file types
+blacklist_file_extensions = [".sfv", ".ds_store", ".md5", ".m3u", ".cue"]	# ignore these file types
 blacklist_path_matches = ["proof", "sample"] # ignore subfolders and filenames containing these strings
 blacklist_media_extensions = [".sub"] # do not extract metainfo from these file extensions
 whitelist_video_extensions = [".mkv", ".avi", ".mp4"] # always extract metainfo from these file extensions
