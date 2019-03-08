d3d5c69b92f3c3b3237066b093e4d9f8784a9885
spiritualized <contact@spiritualized.io>
2019-03-08 00:00:39 -0500

    Add handler auto update

--- basehandler.py
+++ basehandler.py
@@ -1,13 +1,20 @@
-import os
+import os, sys
 
 
 class BaseHandler:
 
+	handler_version = "1.0.0"
 	description = ""
 	options = []
 
+	def get_filename(self):
+		return os.path.basename(sys.modules[self.__module__].__file__)
+
 	def attach_arguments(self, argparser):
 		pass
+
+	def get_update(self, smarthash_version):
+		return ""
 
 	def check_parameters(self, args):
 		pass
