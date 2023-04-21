a64d1ff5c952882af01345917255ae32954d9cb9
spiritualized <contact@spiritualized.io>
2023-04-21 18:02:48 -0400

    Raise PluginError if a plugin cannot be initialized successfully

--- smarthash.py
+++ smarthash.py
@@ -64,13 +64,14 @@ class SmartHash:
         unique_params = {}
 
         for x in plugin_filenames:
-            self.plugins[x] = importlib.import_module("Plugins." + x).SmarthashPlugin()
+            try:
+                self.plugins[x] = importlib.import_module("Plugins." + x).SmarthashPlugin()
 
-            if self.plugins[x].title in self.config:
-                self.plugins[x].set_config(self.config[self.plugins[x].title])
-
-            if not hasattr(self.plugins[x], 'handle'):
-                self.init_error("Could not import \"{0}\" plugin".format(x))
+                if self.plugins[x].title in self.config:
+                    self.plugins[x].set_config(self.config[self.plugins[x].title])
+            except PluginError as e:
+                logging.error(f"Could not import '{x}' plugin: {e.error}")
+                del self.plugins[x]
                 continue
 
             if self.plugins[x].title not in self.config:
