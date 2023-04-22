93ea75dbbf6133a38483b7567f268801ab70d774
spiritualized <contact@spiritualized.io>
2023-04-22 13:45:54 -0400

    Linter fixes

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 
-blacklist_file_extensions = [".sfv", ".ds_store", ".md5", ".m3u", ".cue"]	# ignore these file types
-blacklist_path_matches = ["proof", "sample"] # ignore subfolders and filenames containing these strings
-blacklist_media_extensions = [".sub"] # do not extract metainfo from these file extensions
-whitelist_video_extensions = [".mkv", ".avi", ".mp4"] # always extract metainfo from these file extensions
-whitelist_audio_extensions = [".mp3", ".flac"] # always extract metainfo from these file extensions
+blacklist_file_extensions = [".sfv", ".ds_store", ".md5", ".m3u", ".cue"]  # ignore these file types
+blacklist_path_matches = ["proof", "sample"]  # ignore subfolders and filenames containing these strings
+blacklist_media_extensions = [".sub"]  # do not extract metainfo from these file extensions
+whitelist_video_extensions = [".mkv", ".avi", ".mp4"]  # always extract metainfo from these file extensions
+whitelist_audio_extensions = [".mp3", ".flac"]  # always extract metainfo from these file extensions
 
 requests_retry_interval = 5  # seconds
 
