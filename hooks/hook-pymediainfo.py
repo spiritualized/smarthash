fed1f3bc9c44c1b7c92dadb7d0c2f44eb15ce967
spiritualized <contact@spiritualized.io>
2023-04-22 12:56:07 -0400

    Remove PyInstaller hook for pymediainfo, maintained externally

--- hooks/hook-pymediainfo.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-# coding=utf-8
-
-from PyInstaller.utils.hooks import collect_dynamic_libs
-from PyInstaller.utils.hooks import PY_DYLIB_PATTERNS
-
-PY_DYLIB_PATTERNS.extend(['lib*.so.[0-9]*'])
-
-binaries = collect_dynamic_libs('pymediainfo')
